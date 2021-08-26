25e2c0f6442805a098cdc7ff5035cff981f832d8
KE8AFF <40017587+KE8AFF@users.noreply.github.com>
2021-08-26 09:58:32 -0400

    Create update_upgrade.sh

---
+++ update_upgrade.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-update
+sudo apt-get full-upgrade
+sudo apt-get dist-upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-clean -y
+sudo reboot
+
+fi
